929768032b446fa50dcae8a5f8704aeff21f5c21
Prabhat Kumar <prabhat8065@gmail.com>
2026-06-15 18:13:57 +0530

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.py
@@ -0,0 +1,26 @@
+class Solution:
+    def topKFrequent(self, nums, k):
+
+        # Store frequency of each number
+        count = {}
+
+        for num in nums:
+            if num in count:
+                count[num] += 1
+            else:
+                count[num] = 1
+
+       
+        sorted_items = sorted(
+            count.items(),
+            key=lambda item: item[1],
+            reverse=True
+        )
+
+        result = []
+
+        # Take first k elements
+        for i in range(k):
+            result.append(sorted_items[i][0])
+
+        return result
